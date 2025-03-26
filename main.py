88e82c1b86211ad947c5db0bcb8a49ec4d844af0
Ri Ga <riga1897@yandex.ru>
2025-03-26 22:31:57 +0300

    first test

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from src.widget import get_date, mask_account_card
+from src.processing import filter_by_state
 
+
+
+#from src.widget import get_date, mask_account_card
+
+'''
 input_data = """Maestro 1596837868705199
 Счет 64686473678894779589
 MasterCard 7158300734726758
@@ -11,12 +16,14 @@ Visa Gold 5999414228426353
 
 input_date = "2024-03-11T02:26:18.671407"
 
+for get_string in input_data.splitlines():
+    print(mask_account_card(get_string))
+print(f"{input_date} -> {get_date(input_date)}")
+'''
+
 input_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
               {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
               {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
               {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-for get_string in input_data.splitlines():
-    print(mask_account_card(get_string))
-print(f"{input_date} -> {get_date(input_date)}")
-
+print(filter_by_state(input_list))
--- src/processing.py
+++ src/processing.py
@@ -2,7 +2,7 @@ def filter_by_state(my_list, state='EXECUTED'):
     print(my_list, state)
     pass
 
-def sort_by_date(my_list, reverse=False):
+def sort_by_date(my_list, reverse=True):
     print(my_list, reverse)
 
 if __name__ == "__main__":
