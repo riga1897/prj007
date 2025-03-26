5b525e9477d4553988cf25972291d07269c27d80
Ri Ga <riga1897@yandex.ru>
2025-03-26 22:14:51 +0300

    Prepare files and functions

---
+++ src/processing.py
@@ -0,0 +1,9 @@
+def filter_by_state(my_list, state='EXECUTED'):
+    print(my_list, state)
+    pass
+
+def sort_by_date(my_list, reverse=False):
+    print(my_list, reverse)
+
+if __name__ == "__main__":
+    print("Модуль импортируется из main")
