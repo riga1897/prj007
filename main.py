65c86594388e7f56c151c0a13590a3c45fc7bf0f
Ri Ga <riga1897@yandex.ru>
2025-03-27 09:53:50 +0300

    Finish coddding

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from src.processing import filter_by_state
-
-
-
-#from src.widget import get_date, mask_account_card
-
+from src.processing import filter_by_state, sort_by_date
 '''
+from src.widget import get_date, mask_account_card
+
 input_data = """Maestro 1596837868705199
 Счет 64686473678894779589
 MasterCard 7158300734726758
@@ -15,15 +12,24 @@ Visa Gold 5999414228426353
 Счет 73654108430135874305"""
 
 input_date = "2024-03-11T02:26:18.671407"
-
-for get_string in input_data.splitlines():
-    print(mask_account_card(get_string))
-print(f"{input_date} -> {get_date(input_date)}")
 '''
-
 input_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
               {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
               {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
               {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
+'''
+for get_string in input_data.splitlines():
+    print(mask_account_card(get_string))
+
+print()
+
+print(f"{input_date} -> {get_date(input_date)}")
+
+print()
+'''
 print(filter_by_state(input_list))
+
+print()
+
+print(sort_by_date(input_list))
--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,31 @@
-def filter_by_state(my_list, state='EXECUTED'):
-    print(my_list, state)
-    pass
+from typing import List
 
-def sort_by_date(my_list, reverse=True):
-    print(my_list, reverse)
+def filter_by_state(my_list: List, my_state='EXECUTED') -> List:
+    """
+    Функция фильтрует исходный список по значению параметра 'state'.
+    Если параметр не задан, то фильтрация по значению 'EXECUTED'
+    """
+    filtered_list = [item for item in my_list if item['state'] == my_state]
+
+    return filtered_list
+
+
+def sort_by_date(my_list: List, descending=True) -> List:
+    """
+    Функция сортирует исходный список по значению параметра 'date'
+    Направление сортировки задается параметром 'ascending'
+    Если значение параметра True или параметр не задан, но сортировка по убыванию.
+    Если задан параметр False, то сортировка по возрастанию.
+    """
+    return sorted(my_list,  key=lambda item: item.get("date"), reverse = descending)
+
 
 if __name__ == "__main__":
-    print("Модуль импортируется из main")
+    #print("Модуль импортируется из main")
+    input_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    
+    print (filter_by_state(input_list))
+    print(sort_by_date(input_list))
